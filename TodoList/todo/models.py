928336486007ea6d8e927bc42f61332636209b31
SylvioDev <rakotomangantonio@gmail.com>
2023-12-27 10:04:44 +0300

    Add TODO functionality with category support
    - Created models for Category and TodoItem
    - Implemented the necessary fields for tasks and categories
    - Established a foreign key relationship between TodoItem and Category
    - Updated views and templates to handle CRUD operations for TODO items
    - Tested and ensured proper functioning of the TODO feature

--- TodoList/todo/models.py
+++ TodoList/todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Category class 
+
+class Category(models.Model):
+    name = models.CharField(max_length = 55)
+    
+    def __str__(self):
+        return self.name
+
+# Todo class
+
+class Todo(models.Model):
+    title = models.CharField(max_length = 40)
+    completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    category = models.ForeignKey(Category, 
+                                on_delete = models.SET_NULL, 
+                                blank = True, 
+                                null = True)
+
+    def __str__(self):
+        return self.title
+    
